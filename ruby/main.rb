f63817c7a10fdae02566d98bd47345190cee0035
Arkadiusz Adamczyk <110768379+PontifexusXO@users.noreply.github.com>
2025-06-13 13:41:45 +0200

    feat: add Ruby 3.5

--- ruby/main.rb
+++ ruby/main.rb
@@ -11,7 +11,7 @@ $JUMP_POWER = -15
 $MOVE_SPEED = 5
 $GROUND_LEVEL = 550
 
-$player_initial_x = 50
+$player_initial_x = 0
 $player_initial_y = 350
 $player_velocity = { x: 0, y: 0 }
 $on_ground = false
@@ -20,31 +20,9 @@ $current_anim = :idle
 $player = nil
 $current_scene = "menu"
 $platforms = nil
-
-title = Text.new(
-  "MarioRuby",
-  x: Window.width / 2, 
-  y: Window.height / 2 - 100,
-  size: 50,
-  color: 'white'
-)
-title.x -= title.width / 2
-author = Text.new(
-  "Arkadiusz Adamczyk",
-  x: Window.width / 2, 
-  y: Window.height / 2 - 50,
-  size: 20,
-  color: 'white'
-)
-author.x -= author.width / 2
-subtitle = Text.new(
-  "Press ENTER to start",
-  x: Window.width / 2, 
-  y: Window.height / 2 + 25,
-  size: 25,
-  color: 'white'
-  )
-subtitle.x -= subtitle.width / 2
+$keys = []
+$score = 0
+$score_label = nil
 
 keys = {}
 on :key_held do |event|
@@ -53,11 +31,15 @@ end
 on :key_up do |event|
   if $current_scene == "menu" && event.key == "return"
     start
+  elsif $current_scene == "win" && event.key == "return"
+    menu
   end
   keys[event.key] = false
 end
 
 def create_static_platform(x:, y:, width:, height:, color: 'white')
+  key = Image.new('key.png', x: x + width / 2 - 24, y: y - 48, width: 48, height: 48)
+  $keys << key
   {
     sprite: Rectangle.new(x: x, y: y, width: width, height: height, color: color),
     moving: false
@@ -75,15 +57,44 @@ def create_moving_platform(x:, y:, width:, height:, range:, speed:, color: 'whit
   }
 end
 
-def aabb_collision?(a, b)
-  a.x < b.x + b.width &&
-  a.x + a.width > b.x &&
-  a.y < b.y + b.height &&
-  a.y + a.height > b.y
+def menu
+  clear
+  $current_scene = "menu"
+  title = Text.new(
+    "MarioRuby",
+    x: Window.width / 2, 
+    y: Window.height / 2 - 100,
+    size: 50,
+    color: 'white'
+  )
+  title.x -= title.width / 2
+  author = Text.new(
+    "Arkadiusz Adamczyk",
+    x: Window.width / 2, 
+    y: Window.height / 2 - 50,
+    size: 20,
+    color: 'white'
+  )
+  author.x -= author.width / 2
+  start_game = Text.new(
+    "Press ENTER to start",
+    x: Window.width / 2, 
+    y: Window.height / 2 + 25,
+    size: 25,
+    color: 'white'
+    )
+  start_game.x -= start_game.width / 2
 end
 
 def start
   clear
+  $score_label = Text.new(
+    "Score: 0",
+    x: 24,
+    y: 24,
+    size: 20,
+    color: 'white',
+  )
   $player = Sprite.new(
     'player.png',
     clip_width: 24,
@@ -105,6 +116,44 @@ def start
     create_static_platform(x: 150, y: 175, width: 150, height: 20)
   ]
   $current_scene = "start"
+end
+
+def win
+  clear
+  $current_scene = "win"
+  title = Text.new(
+    "YOU WON",
+    x: Window.width / 2, 
+    y: Window.height / 2 - 100,
+    size: 50,
+    color: 'white'
+  )
+  title.x -= title.width / 2
+  score = Text.new(
+    "Score: #{$score}",
+    x: Window.width / 2, 
+    y: Window.height / 2 - 35,
+    size: 25,
+    color: 'white'
+    )
+  score.x -= score.width / 2
+  menu = Text.new(
+    "Press ENTER to go to menu",
+    x: Window.width / 2, 
+    y: Window.height / 2 + 50,
+    size: 25,
+    color: 'white'
+    )
+  menu.x -= menu.width / 2
+  $score = 0
+  $keys = []
+end
+
+def aabb_collision?(a, b)
+  a.x < b.x + b.width &&
+  a.x + a.width > b.x &&
+  a.y < b.y + b.height &&
+  a.y + a.height > b.y
 end
 
 update do
@@ -167,6 +216,20 @@ update do
       end
     end
 
+    $keys.reject! do |key|
+    if $player.x < key.x + key.width && $player.x + $player.width > key.x && $player.y < key.y + key.height && $player.y + $player.height > key.y
+        key.remove
+        $score += 1
+        $score_label.text = "Score: #{$score}"
+        true
+      else
+        false
+      end
+    end
+    if $keys.length == 0
+      win
+    end
+
     $player.x = [[$player.x, 0].max, Window.width - $player.width].min
     if $player.y > Window.height + 400
       $player.x = $player_initial_x
@@ -177,4 +240,5 @@ update do
   end
 end
 
+menu
 show
